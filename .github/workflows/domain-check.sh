3ad5611aea6a477a318d1891501015f72457cad0
Sophia Garg <56688049+SOPHIA2401@users.noreply.github.com>
2022-06-13 22:43:06 +0530

    modification in CI framework implementation. (#44)
    * GitHub workflow file.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * Modifications in CI workflow file.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * Driver-code and test-case for Ping API is added.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * New-lines added.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * Table added in driver-code for displaying failed result, added comments in ci workflow file and new-line added in ping model test-case.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * command for installing Pretty-table module in python is added.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * minor change-2
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * modification in CI framework implementation.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    * Adding test-case for Ping-API.
    
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>
    
    Co-authored-by: Sophia Garg <sofigarg@amazon.com>

--- .github/workflows/domain-check.sh
+++ .github/workflows/domain-check.sh
@@ -1,5 +1,5 @@
 counter=1
-for counter in {1..10}
+while [ $counter -le 10 ]
 do
     if [ $(curl -s -o /dev/null --head -w "%{http_code}" 'https://admin:admin@localhost:9200' -H 'Content-Type:application/json' --insecure -v) -ne 200 ]; then
         sleep 30s
@@ -8,7 +8,8 @@ do
         echo  "Waiting for addtional 30 seconds.. for Opensearch domain to be up."
         sleep 30s
         break 
-    fi  
+    fi 
+    counter=$(( $counter + 1 )) 
 done
 if [ $counter -eq 11 ]; then
     echo "Unable to connect with OpenSearch URL https://localhost:9200/"
