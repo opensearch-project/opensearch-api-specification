5b60df095904d1dd982d6b715a5610aca98d6912
Sophia Garg <sofigarg@amazon.com>
2022-06-14 11:03:56 +0530

    Error resolving: list index out of range
    Signed-off-by: Sophia Garg <sofigarg@amazon.com>

--- test/scripts/driver-code.py
+++ test/scripts/driver-code.py
@@ -49,10 +49,6 @@ class Dredd:
             curr_path = dirpath.split('/')
             curr_dir = curr_path[len(curr_path)-1]
             if files:
-                print("\n ----------------- \n")
-                print(dirpath)
-                print(curr_dir)
-                print(files)
                 command = "dredd " + dirpath +"/"+ files[1]+ " " + self.endpoint+ " --user=" + self.user + " --hookfiles=" + dirpath + "/" + files[0]
                 if self.test_name != "":
                     if self.test_name == curr_dir:
