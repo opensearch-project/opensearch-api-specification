52d716acc6aa1f4b35782112ce559a4a7c9b61c6
dblock <dblock@amazon.com>
2024-04-02 15:47:16 -0400

    Customize GitHub pages workflow.
    Signed-off-by: dblock <dblock@amazon.com>

---
+++ _plugins/openapi.rb
@@ -0,0 +1,16 @@
+module OpenAPI
+  def self.generate(_site, _payload)
+    return if @generated
+
+    Dir.chdir('tools') do
+      system 'npm install'
+      system 'npm run merge -- ../spec/opensearch-openapi.yaml ../_site/opensearch-openapi.yaml'
+    end
+
+    @generated = true
+  end
+end
+
+Jekyll::Hooks.register :site, :post_write do |site, payload|
+  OpenAPI.generate(site, payload)
+end
