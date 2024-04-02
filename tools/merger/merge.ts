73d3c1e7062c8773d0a2d7bd1f5891d531931ce9
dblock <dblock@amazon.com>
2024-04-02 08:44:34 -0400

    Rename OpenSearch.openapi.yaml to opensearch-openapi.yaml.
    Signed-off-by: dblock <dblock@amazon.com>

--- tools/merger/merge.ts
+++ tools/merger/merge.ts
@@ -2,8 +2,8 @@ import OpenApiMerger from "./OpenApiMerger";
 
 
 async function main() {
-    const root_path: string = process.argv[2]; // '../spec/OpenSearch.openapi.yaml'
-    const output_path: string = process.argv[3]; // '../build/OpenSearch.latest.yaml'
+    const root_path: string = process.argv[2]; // '../spec/opensearch-openapi.yaml'
+    const output_path: string = process.argv[3]; // '../build/opensearch-openapi.yaml'
     const merger = new OpenApiMerger(root_path);
     merger.merge(output_path);
 }
