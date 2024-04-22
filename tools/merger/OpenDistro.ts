674084e81b559b4795e9ba212d4d3dfff86ecf00
Theo Truong <theotr@amazon.com>
2024-04-22 16:51:40 -0700

    # Set Tabsize from 4 to 2
    Signed-off-by: Theo Truong <theotr@amazon.com>

--- tools/merger/OpenDistro.ts
+++ tools/merger/OpenDistro.ts
@@ -1,25 +1,25 @@
 import fs from "fs";
 import YAML from "yaml";
-import { OperationPath, HttpVerb, SupersededOperationMap } from "../types";
-import { write2file } from "../helpers";
+import {HttpVerb, OperationPath, SupersededOperationMap} from "../types";
+import {write2file} from "../helpers";
 
 // One-time script to generate _superseded_operations.yaml file for OpenDistro
 // Keeping this for now in case we need to update the file in the near future. Can be removed after a few months.
 // TODO: Remove this file in 2025.
 export default class OpenDistro {
-    input: Record<OperationPath, HttpVerb[]>;
-    output: SupersededOperationMap = {};
+  input: Record<OperationPath, HttpVerb[]>;
+  output: SupersededOperationMap = {};
 
-    constructor(file_path: string) {
-        this.input = YAML.parse(fs.readFileSync(file_path, 'utf8'));
-        this.build_output();
-        write2file(file_path, this.output);
-    }
+  constructor(file_path: string) {
+    this.input = YAML.parse(fs.readFileSync(file_path, 'utf8'));
+    this.build_output();
+    write2file(file_path, this.output);
+  }
 
-    build_output() {
-        for(const [path, operations] of Object.entries(this.input)) {
-            const replaced_by = path.replace('_opendistro', '_plugins');
-            this.output[path] = { superseded_by: replaced_by, operations };
-        }
+  build_output() {
+    for (const [path, operations] of Object.entries(this.input)) {
+      const replaced_by = path.replace('_opendistro', '_plugins');
+      this.output[path] = {superseded_by: replaced_by, operations};
     }
+  }
 }
--- tools/types.ts
+++ tools/types.ts
@@ -1,30 +1,30 @@
 import {OpenAPIV3} from "openapi-types";
 
 export interface OperationSpec extends OpenAPIV3.OperationObject {
-    'x-operation-group': string;
-    'x-version-added': string;
-    'x-version-removed'?: string;
-    'x-version-deprecated'?: string;
-    'x-deprecation-message'?: string;
-    'x-ignorable'?: boolean;
+  'x-operation-group': string;
+  'x-version-added': string;
+  'x-version-removed'?: string;
+  'x-version-deprecated'?: string;
+  'x-deprecation-message'?: string;
+  'x-ignorable'?: boolean;
 
-    parameters?: OpenAPIV3.ReferenceObject[];
-    requestBody?: OpenAPIV3.ReferenceObject;
-    responses: { [code: string]: OpenAPIV3.ReferenceObject };
+  parameters?: OpenAPIV3.ReferenceObject[];
+  requestBody?: OpenAPIV3.ReferenceObject;
+  responses: { [code: string]: OpenAPIV3.ReferenceObject };
 }
 
 export interface ParameterSpec extends OpenAPIV3.ParameterObject {
-    schema: OpenAPIV3.SchemaObject | OpenAPIV3.ReferenceObject;
-    'x-data-type'?: string;
-    'x-version-deprecated'?: string;
-    'x-deprecation-message'?: string;
-    'x-global'?: boolean;
+  schema: OpenAPIV3.SchemaObject | OpenAPIV3.ReferenceObject;
+  'x-data-type'?: string;
+  'x-version-deprecated'?: string;
+  'x-deprecation-message'?: string;
+  'x-global'?: boolean;
 }
 
 export interface ValidationError {
-    file: string;
-    location?: string;
-    message: string;
+  file: string;
+  location?: string;
+  message: string;
 }
 
 export type HttpVerb = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD' | 'OPTIONS' | 'TRACE'
