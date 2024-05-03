8b758869f3548b0e432f5426d19c857e52622b1e
Theo Truong <theotr@amazon.com>
2024-05-03 17:39:55 -0600

    Added validation for _info.yaml DRY'ed JSON schema validation logic
    Signed-off-by: Theo Truong <theotr@amazon.com>

--- tools/linter/SpecValidator.ts
+++ tools/linter/SpecValidator.ts
@@ -3,15 +3,18 @@ import NamespacesFolder from './components/NamespacesFolder'
 import { type ValidationError } from '../types'
 import SchemaRefsValidator from './SchemaRefsValidator'
 import SupersededOperationsFile from './components/SupersededOperationsFile'
+import InfoFile from './components/InfoFile'
 
 export default class SpecValidator {
-  superseded_ops_files: SupersededOperationsFile
+  superseded_ops_file: SupersededOperationsFile
+  info_file: InfoFile
   namespaces_folder: NamespacesFolder
   schemas_folder: SchemasFolder
   schema_refs_validator: SchemaRefsValidator
 
   constructor (root_folder: string) {
-    this.superseded_ops_files = new SupersededOperationsFile(`${root_folder}/_superseded_operations.yaml`)
+    this.superseded_ops_file = new SupersededOperationsFile(`${root_folder}/_superseded_operations.yaml`)
+    this.info_file = new InfoFile(`${root_folder}/_info.yaml`)
     this.namespaces_folder = new NamespacesFolder(`${root_folder}/namespaces`)
     this.schemas_folder = new SchemasFolder(`${root_folder}/schemas`)
     this.schema_refs_validator = new SchemaRefsValidator(this.namespaces_folder, this.schemas_folder)
@@ -26,7 +29,8 @@ export default class SpecValidator {
 
     return [
       ...this.schema_refs_validator.validate(),
-      ...this.superseded_ops_files.validate()
+      ...this.superseded_ops_file.validate(),
+      ...this.info_file.validate()
     ]
   }
 }
