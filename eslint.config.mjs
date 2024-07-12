02012623439c06f38c61268eb648b19dbb749c18
dblock <dblock@amazon.com>
2024-07-12 10:32:34 -0400

    Enabled yml/sort-sequence-values.
    Signed-off-by: dblock <dblock@amazon.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -118,7 +118,8 @@ export default [
       'yml/plain-scalar': ['error', 'always'],
       'yml/no-trailing-zeros': 'error',
       'yml/no-multiple-empty-lines': 'error',
-      '@cspell/spellchecker': ['error', { customWordListFile: '.cspell', autoFix: true }]
+      '@cspell/spellchecker': ['error', { customWordListFile: '.cspell', autoFix: true }],
+      'yml/sort-sequence-values': ['error', { pathPattern: '.*', order: { type: 'asc' } }]
     }
   }
 ]
