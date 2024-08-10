33a589f55717b5c29b13455ff02a6b4cc68f0ec7
Andy Wick <andywick@amazon.com>
2024-08-10 16:19:39 -0400

    Remove EXCLUDED_FILES instead only process .yaml
    Signed-off-by: Andy Wick <andywick@amazon.com>

--- tools/src/tester/TestRunner.ts
+++ tools/src/tester/TestRunner.ts
@@ -20,10 +20,6 @@ import { OpenSearchHttpClient } from 'OpenSearchHttpClient'
 import * as ansi from './Ansi'
 import _ from 'lodash'
 
-const EXCLUDED_FILES = [
-  'docker-compose.yml'
-]
-
 export default class TestRunner {
   private readonly _http_client: OpenSearchHttpClient
   private readonly _story_validator: StoryValidator
@@ -80,9 +76,7 @@ export default class TestRunner {
       }]
     } else {
       return _.compact(fs.readdirSync(path).flatMap(next_file => {
-        if (!EXCLUDED_FILES.includes(next_file)) {
-          return this.#collect_story_files(path, next_file, next_prefix)
-        }
+        return this.#collect_story_files(path, next_file, next_prefix)
       }))
     }
   }
