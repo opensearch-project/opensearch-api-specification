6b641b418fc7f8f217c55cbe07389839ac53d481
dblock <dblock@amazon.com>
2024-09-09 09:07:30 -0400

    Fix: delete leftoever undefined array items.
    Signed-off-by: dblock <dblock@amazon.com>

--- tools/tests/merger/OpenApiVersionExtractor.test.ts
+++ tools/tests/merger/OpenApiVersionExtractor.test.ts
@@ -43,7 +43,7 @@ describe('extract() from a merged API spec', () => {
     test('has matching responses', () => {
       const spec = extractor.extract()
       expect(_.keys(spec.paths['/index']?.get?.responses)).toEqual([
-        '200', '201', '404', '500', '503', 'removed-2.0', 'added-1.3-removed-2.0', 'distributed-excluded-amazon-serverless'
+        '200', '201', '404', '500', '503', 'removed-2.0', 'removed-2.0-refs', 'added-1.3-removed-2.0', 'distributed-excluded-amazon-serverless'
       ])
     })
   })
@@ -54,7 +54,7 @@ describe('extract() from a merged API spec', () => {
     test('has matching responses', () => {
       const spec = extractor.extract()
       expect(_.keys(spec.paths['/index']?.get?.responses)).toEqual([
-        '200', '201', '404', '500', '503', 'added-2.0', 'distributed-excluded-amazon-serverless'
+        '200', '201', '404', '500', '503', 'added-2.0', 'removed-2.0-refs', 'distributed-excluded-amazon-serverless'
       ])
     })
 
@@ -86,7 +86,7 @@ describe('extract() from a merged API spec', () => {
     test('has matching responses', () => {
       const spec = extractor.extract()
       expect(_.keys(spec.paths['/index']?.get?.responses)).toEqual([
-        '200', '201', '404', '500', '503', 'added-2.0', 'added-2.1', 'distributed-excluded-amazon-serverless'
+        '200', '201', '404', '500', '503', 'added-2.0', 'removed-2.0-refs', 'added-2.1', 'distributed-excluded-amazon-serverless'
       ])
     })
   })
