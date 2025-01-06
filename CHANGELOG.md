e7329b63669c5cf50fa93d167c3773762855558a
dblock <dblock@amazon.com>
2025-01-06 08:53:07 -0500

    Upload test spec logs.
    Signed-off-by: dblock <dblock@amazon.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -44,6 +44,7 @@ Inspired from [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 - Added `PUT /_plugins/_ml/model_groups/{model_group_id}`, `GET /_plugins/_ml/model_groups/_search`, and `POST /_plugins/_ml/model_groups/_search` ([#760](https://github.com/opensearch-project/opensearch-api-specification/pull/760))
 - Added `GET /_plugins/_ml/connectors/{connector_id}`, `_search`, `POST /_plugins/_ml/connectors/_search`, and `PUT /_plugins/_ml/connectors/{connector_id}` ([#764](https://github.com/opensearch-project/opensearch-api-specification/pull/764))
 - Added the ability to skip an individual chapter test ([#765](https://github.com/opensearch-project/opensearch-api-specification/pull/765))
+- Added uploading of test spec logs ([#767](https://github.com/opensearch-project/opensearch-api-specification/pull/767))
 
 ### Removed
 - Removed unsupported `_common.mapping:SourceField`'s `mode` field and associated `_common.mapping:SourceFieldMode` enum ([#652](https://github.com/opensearch-project/opensearch-api-specification/pull/652))
--- tools/src/Logger.ts
+++ tools/src/Logger.ts
@@ -7,6 +7,9 @@
 * compatible open source license.
 */
 
+import * as fs from 'fs'
+import * as path from 'path'
+
 export enum LogLevel {
   error = 1,
   warn = 2,
@@ -15,6 +18,7 @@ export enum LogLevel {
 
 export class Logger {
   level: LogLevel
+  log_file?: string
 
   static messages = {
     [LogLevel.error]: 'ERROR',
@@ -22,29 +26,45 @@ export class Logger {
     [LogLevel.info]: 'INFO'
   }
 
-  constructor (level: LogLevel = LogLevel.warn) {
+  constructor(level: LogLevel = LogLevel.warn, log_file?: string) {
     this.level = level
+    this.log_file = log_file
+
+    if (this.log_file !== undefined) {
+      const logs_path = path.dirname(this.log_file)
+      if (!fs.existsSync(logs_path)) {
+        fs.mkdirSync(logs_path, { recursive: true })
+      }
+    }
   }
 
-  log (message: string): void {
+  log(message: string): void {
     console.log(message)
+    this.#write(message)
   }
 
-  info (message: string): void {
+  info(message: string): void {
     this.#log(LogLevel.info, message)
   }
 
-  warn (message: string): void {
+  warn(message: string): void {
     this.#log(LogLevel.warn, message)
   }
 
-  error (message: string): void {
+  error(message: string): void {
     this.#log(LogLevel.error, message)
   }
 
-  #log (level: LogLevel, message: string): void {
-    if (level > this.level) return
+  #log(level: LogLevel, message: string): void {
     const output = `[${Logger.messages[level]}] ${message}`
+    this.#write(output)
+    if (level > this.level) return
     console.log(output)
+  }
+
+  #write(message: string): void {
+    if (this.log_file !== undefined) {
+      fs.appendFileSync(this.log_file, message + '\n')
+    }
   }
 }
