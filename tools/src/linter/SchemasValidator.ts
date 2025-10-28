8f4b485f22e80570fa2370b5a5c349897758ac12
lucy66hw <fridalu66@gmail.com>
2025-10-28 10:50:43 -0700

    Added x-protobuf-excluded vendor extension (#980)
    Signed-off-by: xil <fridalu66@gmail.com>
    Co-authored-by: xil <xil@uber.com>

--- tools/src/linter/SchemasValidator.ts
+++ tools/src/linter/SchemasValidator.ts
@@ -20,7 +20,8 @@ const ADDITIONAL_KEYWORDS = [
   'x-distributions-included',
   'x-distributions-excluded',
   'x-supports-typed-keys',
-  'x-is-generic-type-parameter'
+  'x-is-generic-type-parameter',
+  'x-protobuf-excluded'
 ]
 
 export default class SchemasValidator {
